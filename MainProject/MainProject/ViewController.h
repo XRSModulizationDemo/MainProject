3ff5cccd86aefbafbd3af7c70d26605150a6bcd4
jason_xu <632317684@qq.com>
2017-03-31 11:00:12 +0800

    提交主工程

---
+++ MainProject/MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by 徐强 on 17/3/31.
+//  Copyright © 2017年 徐强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MainProject/MainProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MainProject
+//
+//  Created by 徐强 on 17/3/31.
+//  Copyright © 2017年 徐强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
